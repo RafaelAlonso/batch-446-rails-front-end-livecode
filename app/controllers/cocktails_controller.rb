01712442a29e257726bb1ae2398aa2407e5b9d8d
Rafael Alonso <rafael.palonso@gmail.com>
2020-08-21 14:38:37 -0300

    Rails Front-End livecodde batch #446

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -10,6 +10,28 @@ class CocktailsController < ApplicationController
   def show
     # pegar o cocktail especifico pra passar para a view
     @cocktail = Cocktail.find(params[:id])
+  end
 
+  # a ideia do new e mostrar o form de criacao de um cocktail
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    # se salvarmos no bd
+    if @cocktail.save
+      # mando ele para o show
+      redirect_to cocktail_path(@cocktail.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :cocktails, only: [:index, :show]
+  resources :cocktails, only: [:index, :show, :new, :create] do
+
+    # quero criar as rotas do apocalipse de doses
+    # tendo o id do cocktail na url
+    resources :doses, only: [:new, :create]
+  end
+
   # 7 rotas do apocalipse (CRUD)
   # index
   # show
